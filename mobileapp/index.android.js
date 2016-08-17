4d6fa916a101dac838fd52755f39294c7ef994dc
Manu Viswam <evmanu@gmail.com>
2016-08-17 17:37:47 +0530

    Manu | Created a location watcher as initial step

--- mobileapp/index.android.js
+++ mobileapp/index.android.js
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
@@ -13,11 +8,48 @@ import {
 } from 'react-native';
 
 class CharchaPoint extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+          initialPosition: 'unknown',
+          lastPosition: 'unknown',
+        };
+    }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        alert(initialPosition);
+        this.setState({initialPosition});
+      },
+      (error) => alert(error),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({lastPosition});
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to Charcha Point!
+          New
+        </Text>
+        <Text>
+          <Text style={styles.welcome}>Initial position: </Text>
+          {this.state.initialPosition}
+        </Text>
+        <Text>
+          <Text style={styles.welcome}>Current position: </Text>
+          {this.state.lastPosition}
         </Text>
       </View>
     );
