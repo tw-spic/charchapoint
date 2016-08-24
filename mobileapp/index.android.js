4cbafdf350bbfd219b0972db965e3b3bb5c47bb5
Manu Viswam <evmanu@gmail.com>
2016-08-24 22:41:54 +0530

    Manu | Simple code to receive FCM messages

--- mobileapp/index.android.js
+++ mobileapp/index.android.js
@@ -8,8 +8,33 @@ import {
   TouchableWithoutFeedback
 } from 'react-native';
 import { NativeModules } from 'react-native';
+import FCM from 'react-native-fcm';
 
 class Example extends React.Component {
+  componentDidMount() {
+    FCM.requestPermissions(); // for iOS
+    FCM.getFCMToken().then(token => {
+      console.log(token)
+      // store fcm token in your server
+    });
+    this.notificationUnsubscribe = FCM.on('notification', (notif) => {
+      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+    });
+    this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+      console.log(token)
+      // fcm token may not be available on first load, catch it here
+    });
+
+    FCM.subscribeToTopic('/topics/foo-bar');
+    FCM.unsubscribeFromTopic('/topics/foo-bar');
+  }
+
+  componentWillUnmount() {
+    // prevent leaking
+    this.refreshUnsubscribe();
+    this.notificationUnsubscribe();
+  }
+
   render() {
     NativeModules.MyToastAndroid.show('Awesome', 300);
     return (
