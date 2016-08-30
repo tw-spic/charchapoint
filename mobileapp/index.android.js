c05bd51cc9412f5ce25524bf2cfc4bcafd83ee1c
Manu Viswam <evmanu@gmail.com>
2016-08-30 18:46:05 +0530

    Manu | Simple broadcast messaging using only fcm database

--- mobileapp/index.android.js
+++ mobileapp/index.android.js
@@ -8,31 +8,67 @@ import {
   TouchableWithoutFeedback
 } from 'react-native';
 import { NativeModules } from 'react-native';
-import FCM from 'react-native-fcm';
+// import FCM from 'react-native-fcm';
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "masked",
+  authDomain: "1028630304114-nec2l2fmac4p32dke638rofrv9u1q9tt.apps.googleusercontent.com",
+  databaseURL: "charcha-point.firebaseio.com",
+  storageBucket: "charcha-point.appspot.com"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class Example extends React.Component {
-  componentDidMount() {
-    FCM.requestPermissions(); // for iOS
-    FCM.getFCMToken().then(token => {
-      console.log(token)
-      // store fcm token in your server
-    });
-    this.notificationUnsubscribe = FCM.on('notification', (notif) => {
-      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
-    });
-    this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
-      console.log(token)
-      // fcm token may not be available on first load, catch it here
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "hello"
+    }
 
-    FCM.subscribeToTopic('/topics/foo-bar');
-    FCM.unsubscribeFromTopic('/topics/foo-bar');
+  }
+  // componentDidMount() {
+  //   FCM.requestPermissions(); // for iOS
+  //   FCM.getFCMToken().then(token => {
+  //     console.log(token)
+  //     // store fcm token in your server
+  //   });
+  //   this.notificationUnsubscribe = FCM.on('notification', (notif) => {
+  //     this.setState({
+  //       data: this.state.data + "\n" + notif.message
+  //     })
+  //     // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+  //   });
+  //   this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+  //     console.log(token)
+  //     // fcm token may not be available on first load, catch it here
+  //   });
+
+  //   FCM.subscribeToTopic('/topics/foo-bar');
+  //   FCM.unsubscribeFromTopic('/topics/foo-bar');
+  // }
+
+  componentDidMount() {
+      firebase.database().ref('messages').set({});
+    var messagesRef = firebase.database().ref('messages');
+    messagesRef.on('child_added', (msg) => {
+      console.log(msg.val());
+      this.setState({data: this.state.data + "\n" + msg.key +" : "+ msg.val().message});
+    });
+  }
+
+  _clickPost(){
+    NativeModules.MyToastAndroid.show('Awesome', 300);
+  firebase.database().ref('messages').push({
+        message: "hello from nexus 5"
+      });
   }
 
   componentWillUnmount() {
-    // prevent leaking
-    this.refreshUnsubscribe();
-    this.notificationUnsubscribe();
+    // // prevent leaking
+    // this.refreshUnsubscribe();
+    // this.notificationUnsubscribe();
   }
 
   render() {
@@ -40,9 +76,8 @@ class Example extends React.Component {
     return (
       <View>
         <TouchableWithoutFeedback
-          onPress={() =>
-            NativeModules.MyToastAndroid.show('Awesome', 300)}>
-          <Text>Click me.</Text>
+          onPress={() => this._clickPost()}>
+          <Text>Click me. {this.state.data}</Text>
         </TouchableWithoutFeedback>
       </View>
     );
