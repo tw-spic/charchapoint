806147ea0af3cec2e945d29a9326e5a54575be30
Manu Viswam <evmanu@gmail.com>
2016-09-20 01:20:57 +0530

    Manu | very basic version of hardcoded zones and messaging based on zones

--- mobileapp/index.android.js
+++ mobileapp/index.android.js
@@ -5,12 +5,14 @@ import {
   Text,
   View,
   AppRegistry,
+  ToastAndroid,
   TouchableWithoutFeedback
 } from 'react-native';
 import { NativeModules } from 'react-native';
 // import FCM from 'react-native-fcm';
 import * as firebase from 'firebase'
 import { GiftedChat } from 'react-native-gifted-chat';
+import geodist from 'geodist';
 
 const firebaseConfig = {
   apiKey: "masked",
@@ -19,6 +21,39 @@ const firebaseConfig = {
   storageBucket: "charcha-point.appspot.com"
 };
 
+const zones = [{ // get zones from server
+  Id:123,
+  Name:"Thoughtworks",
+  Description:"TW Bangalore",
+  Lat:12.928843,
+  Long:77.628614,
+  Radius:500
+},
+{
+  Id:124,
+  Name:"Home",
+  Description:"My Home",
+  Lat:12.930197,
+  Long:77.634091,
+  Radius:500
+},
+{
+  Id:125,
+  Name:"TW Pune",
+  Description:"Thoughtworks",
+  Lat:18.555910,
+  Long:73.891793,
+  Radius:500
+},
+{
+  Id:125,
+  Name:"Sony signal",
+  Description:"Sony signal",
+  Lat:12.937292,
+  Long:77.626935,
+  Radius:500
+}]
+ 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class Example extends React.Component {
@@ -26,27 +61,9 @@ class Example extends React.Component {
     super(props);
     this.state = {messages: []};
     this.onSend = this.onSend.bind(this);
+    this.findCurrentZone = this.findCurrentZone.bind(this);
+    this.subscribeMessages = this.subscribeMessages.bind(this);
   }
-  // componentDidMount() {
-  //   FCM.requestPermissions(); // for iOS
-  //   FCM.getFCMToken().then(token => {
-  //     console.log(token)
-  //     // store fcm token in your server
-  //   });
-  //   this.notificationUnsubscribe = FCM.on('notification', (notif) => {
-  //     this.setState({
-  //       data: this.state.data + "\n" + notif.message
-  //     })
-  //     // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
-  //   });
-  //   this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
-  //     console.log(token)
-  //     // fcm token may not be available on first load, catch it here
-  //   });
-
-  //   FCM.subscribeToTopic('/topics/foo-bar');
-  //   FCM.unsubscribeFromTopic('/topics/foo-bar');
-  // }
 
   componentWillMount() {
     this.deviceId = Math.trunc(Math.random() * 100); // Some random id for now
@@ -56,8 +73,31 @@ class Example extends React.Component {
   }
 
   componentDidMount() {
-    firebase.database().ref('messages').set({});
-    var messagesRef = firebase.database().ref('messages');
+
+    //register location watcher
+    navigator.geolocation.getCurrentPosition(
+      (position) => {},
+      (error) => alert(error),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      var currZone = this.findCurrentZone(lat,long);
+      ToastAndroid.show("Current zone is " + currZone.Name, ToastAndroid.LONG);
+      this.setState((previousState) => {
+        return {
+          ...previousState,
+          zone:currZone
+        };
+      });
+      this.subscribeMessages();
+    });
+  }
+
+  subscribeMessages() {
+    var messagesRef = firebase.database().ref("messages/" + this.state.zone.Id);
     messagesRef.on('child_added', (msg) => {
       this.setState((previousState) => {
         return {
@@ -65,20 +105,21 @@ class Example extends React.Component {
         };
       });
     });
-
   }
 
-  onSend(messages = []) {
-    firebase.database().ref('messages').push({
+  findCurrentZone(lat, long) {
+    return zones.find((zone) => { return geodist({lat:lat,lon:long}, { lat:zone.Lat, lon:zone.Long }, {unit: 'meters'}) < zone.Radius})
+  }
+
+  onSend(messages = []) { // now we are directly writing to the FCM database, we may need to route it through our server to enable FCM notifications
+    firebase.database().ref("messages/" + this.state.zone.Id).push({
         message: messages
     });
     
   }
 
   componentWillUnmount() {
-    // // prevent leaking
-    // this.refreshUnsubscribe();
-    // this.notificationUnsubscribe();
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   render() {
